436209b5e116582717c988d012560235b2672f96
Kevin <stockmannskevin@gmail.com>
2024-08-30 15:32:39 -0300

    remove dist

--- dist/portfolio/server/chunk-QRXNKSHI.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{X as n,eb as o,fb as a,nb as c,pb as p}from"./chunk-WJILESQA.mjs";import"./chunk-5XUXGTUW.mjs";var s=(()=>{class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact-page"]],standalone:!0,features:[p],decls:2,vars:0,template:function(t,i){t&1&&(o(0,"p"),c(1,"contact-page works!"),a())}})}return e})();export{s as ContactPageComponent};
--- dist/portfolio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-4PNA4ZB5.mjs";import"./chunk-BETEDQBP.mjs";import"./chunk-BJLI5HEU.mjs";import"./chunk-BLDDQ52H.mjs";import"./chunk-UIRJGLYL.mjs";import"./chunk-PJO2HVFA.mjs";import"./chunk-VLSTHS6H.mjs";import"./chunk-7MK4VVFR.mjs";import"./chunk-UIUNPF42.mjs";import"./chunk-WJILESQA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
