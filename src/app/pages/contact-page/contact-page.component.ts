695c3fe326a51090c1b5e64f9fa12a9c9cfa9d3c
Kevin <stockmannskevin@gmail.com>
2024-09-03 10:02:07 -0300

    contact page

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../components/title/title.component';
+import { Title } from '@angular/platform-browser';
+import { ButtonComponent } from '../../components/button/button.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, ButtonComponent, ReactiveFormsModule, LoaderComponent],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
 export class ContactPageComponent {
+  form:FormGroup
+  loading = false;
 
+  constructor(protected titleService: Title, private formBuilder:FormBuilder,
+    private http: HttpClient
+  ){
+    this.form = formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.maxLength(3000), Validators.minLength(20)]]
+    })
+
+
+    // this.form.events.subscribe(el=>console.log(el))
+  }
+
+
+  getValue(control:string){
+    return this.form.get(control)?.value;
+  }
+  hasOneError(control:string){
+    return this.form.get(control)?.invalid && this.form.get(control)?.touched;
+  }
+  hasError(control:string, error:string){
+    return this.form.get(control)?.getError(error) && this.form.get(control)?.touched;
+  }
+  getError(control:string, error:string){
+    return this.form.get(control)?.getError(error);
+  }
+
+
+  onSubmit(){
+    this.form.markAllAsTouched();
+    if(this.form.valid && !this.loading){
+      this.loading = true;
+      this.http.post('https://formsubmit.co/ajax/stockmannskevin@email.com', {form_data: this.form.value}).subscribe({
+        next: res=>{
+          console.log(res);
+          this.loading = false;
+          
+        },
+        error: err=>{
+          console.log(err);
+          this.loading = false;
+          
+        }
+      });
+    }
+  }
 }
