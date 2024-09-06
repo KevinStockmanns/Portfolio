71ab135f3f3983777d377e59e625d435900f8e06
Kevin <stockmannskevin@gmail.com>
2024-09-06 09:20:11 -0300

    error page

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.css'
 })
