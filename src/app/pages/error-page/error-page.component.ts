5b8d7b0523e74dd397287640bf2441b16da3460a
Kevin <stockmannskevin@gmail.com>
2024-09-09 09:46:52 -0300

    error page

--- src/app/pages/error-page/error-page.component.ts
+++ src/app/pages/error-page/error-page.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-error-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [MatIconModule, ButtonComponent],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.css'
 })
 export class ErrorPageComponent {
   message = 'Parece que la página que quieres visitar no se encuentra.';
+  icon:string = "sentiment_very_dissatisfied";
   
+  constructor(private router: Router){
+    if(router.url == '/offline'){
+      this.icon = 'wifi_off';
+      this.message = 'Parece que no tienes conexión a internet.';
+    }
+  }
 }
